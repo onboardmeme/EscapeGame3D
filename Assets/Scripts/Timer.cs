d675b9ae130ea7be39532849299171a3ae7461ce
onboardmeme <khf006@email.latech.edu>
2026-04-08 22:29:56 -0500

    Adding Countdown Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -12,7 +13,11 @@ public class Timer : MonoBehaviour
 
             int minutes = Mathf.FloorToInt(remainingTime / 60);
             int seconds = Mathf.FloorToInt(remainingTime % 60);
-            TimerText.text = string.Format("{00:00}:{1:00}", minutes, seconds);
+            TimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        } else {
+            remainingTime = 0;
+            TimerText.text = string.Format("You Lose");
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
